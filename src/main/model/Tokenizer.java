234e19ad2e4d80659a1c6ab2bba538a24ccf6ec6
Kyteware <kyteware.biz@gmail.com>
2025-01-30 17:59:17 -0800

    implement tokenizer

--- src/main/model/Tokenizer.java
+++ src/main/model/Tokenizer.java
@@ -1,11 +1,25 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-// A utility class for tokenizing java files 
 public class Tokenizer {
+    private static String stringMatch = "\"(\\\\\"|.)*?\""; // doesn't work with delimited double quotes
+    private static String wordMatch = "([A-Z]|[a-z]|_)([A-Z]|[a-z]|[0-9]|_)*";
+    private static String numberMatch = "[0-9]+\\.?[0-9]*";
+    private static String symbolMatch = "\\!|\\@|\\%|\\^|\\&|\\*|\\(|\\)|\\||\\-|\\+|\\[|\\]|\\{|\\}|\\;\\|\\:|\\,|\\.|\\<|\\>|\\/";
+    private static String[] allMatches = { stringMatch, wordMatch, numberMatch, symbolMatch };
+    private static String tokenMatch = String.join("|", allMatches);
+    private static Pattern pattern = Pattern.compile(tokenMatch);
+
     public static List<String> tokenize(String rawJava) {
-        // stub
-        return null;
+        Matcher matcher = pattern.matcher(rawJava);
+        ArrayList<String> parts = new ArrayList<String>();
+        while (matcher.find()) {
+            parts.add(matcher.group());
+        }
+        return parts;
     }
 }
