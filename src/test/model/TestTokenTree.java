24983b7fd4f568fab61671bcaf07a30a3c561839
Kyteware <kyteware.biz@gmail.com>
2025-02-06 17:45:39 -0800

    revise tokentree tests for new spec

--- src/test/model/TestTokenTree.java
+++ src/test/model/TestTokenTree.java
@@ -10,43 +10,133 @@ import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 public class TestTokenTree {
-    TokenTree flatSingle;
-    TokenTree flatMultiple;
-    TokenTree flatWithParens;
-    List<TokenTree> flatSeveralStatements;
-    TokenTree emptyClass;
-    TokenTree filledClass;
-    List<TokenTree> manyClasses;
+    TokenTree leafSingle;
+    TokenTree leafMultiple;
+    TokenTree branchSemiSeperated;
+    TokenTree branchCommaSeperated;
+    TokenTree branchEmptyBraces;
+    TokenTree branchEmptyBracesDeep;
+    TokenTree arrayDecl;
+    TokenTree fnDecl;
+    TokenTree classDecl;
+    TokenTree javaFile;
 
     @BeforeEach
     public void beforeEach() {
-        String[] flatSingleTokens = { "return" };
-        flatSingle = new TokenTree(Arrays.asList(flatSingleTokens), null);
-        String[] flatMultipleTokens = { "private", "MyClass", "bob"};
-        flatMultiple = new TokenTree(Arrays.asList(flatMultipleTokens), null);
-        List<String> flatWithParensTokens = Tokenizer.tokenize(
-            "jimmy = new Person(jimmySoul)"
-        );
-        flatWithParens = new TokenTree(flatWithParensTokens, null);
-        List<String> emptyClassTokens = Tokenizer.tokenize(
-            "class MyClass"
-        );
-        emptyClass = new TokenTree(emptyClassTokens, new ArrayList<>());
-        List<String> filledClassTokens = Tokenizer.tokenize(
-            "public class Glass { private String aString; }"
-        );
-        filledClass = new TokenTree(filledClassTokens);
-        List<String> manyClassesTokens = Tokenizer.tokenize(
-            "public class C1 { public void myFn() { print(2) } }"
-            + "class C2 { static void OtherFn() {} }"
-        );
-        manyClasses = new TokenTree(manyClassesTokens);
+        leafSingle = TokenTree.parseFlatTokens(Tokenizer.tokenize(
+            "return"
+        ));
+        leafMultiple = TokenTree.parseFlatTokens(Tokenizer.tokenize(
+            "return \"bob\""
+        ));
+        branchSemiSeperated = TokenTree.parseFlatTokens(Tokenizer.tokenize(
+            "int bob; bob=1; return \"bob\";"
+        ));
+        branchCommaSeperated = TokenTree.parseFlatTokens(Tokenizer.tokenize(
+            "1, 2, \"hi\", bob"
+        ));
+        branchEmptyBraces = TokenTree.parseFlatTokens(Tokenizer.tokenize(
+            "[]"
+        ));
+        branchEmptyBracesDeep = TokenTree.parseFlatTokens(Tokenizer.tokenize(
+            "({})"
+        ));
+        arrayDecl = TokenTree.parseFlatTokens(Tokenizer.tokenize(
+            "int[] = {1, 2, 3}"
+        ));
+        javaFile = TokenTree.parseJavaFileTokens(Tokenizer.tokenize(
+            "import java.util.ArrayList;"
+            + "public class Main {"
+            + " public static void main(String[] args) {"
+            + "  System.out.println(\"hiii\");"
+            + " }"
+            + "}"
+        ));
     }
     
     @Test
-    public void testConstructingFlat() {
-        String[] flatSingleContent = { "return" };
-        assertEquals(Arrays.asList(flatSingleContent), flatSingle.getContent());
-        String[] flatMultipleContent = 
+    public void testParseLeaves() {
+        assertTrue(leafSingle.isLeaf());
+        assertEquals("return", leafSingle.getTokens().get(0));
+
+        assertTrue(leafMultiple.isLeaf());
+        assertEquals("return", leafSingle.getTokens().get(0));
+        assertEquals("bob", leafSingle.getTokens().get(1));
+    }
+
+    @Test
+    public void testParseBranches() {
+        assertTrue(branchSemiSeperated.isBranch());
+        assertEquals(TokenTree.SEPERATED_SEMICOLON, branchSemiSeperated.getSeperators());
+        TokenTree statement1 = branchSemiSeperated.getTrees().get(0);
+        assertTrue(statement1.isLeaf());
+        assertEquals("int", statement1.getTokens().get(0));
+        assertEquals("bob", statement1.getTokens().get(1));
+        assertEquals(2, statement1.getTokens().size());
+        assertTrue(branchSemiSeperated.getTrees().get(1).isLeaf());
+        assertTrue(branchSemiSeperated.getTrees().get(2).isLeaf());
+
+        assertTrue(branchCommaSeperated.isBranch());
+        assertEquals(TokenTree.SEPERATED_COMMA, branchCommaSeperated.getSeperators());
+    }
+
+    @Test
+    public void testParseEmpties() {
+        assertTrue(branchEmptyBraces.isBranch());
+        assertEquals(TokenTree.DELIMITED_SQUARY, branchEmptyBraces.getDelimiters());
+        assertEquals(TokenTree.SEPERATED_NA, branchEmptyBraces.getSeperators());
+        assertEquals(0, branchEmptyBraces.getTrees().size());
+
+        assertTrue(branchEmptyBracesDeep.isBranch());
+        assertEquals(TokenTree.DELIMITED_ROUND, branchEmptyBracesDeep.getDelimiters());
+        assertEquals(TokenTree.SEPERATED_NA, branchEmptyBracesDeep.getSeperators());
+        assertEquals(1, branchEmptyBracesDeep.getTrees().size());
+        TokenTree inside = branchEmptyBracesDeep.getTrees().get(0);
+        assertEquals(TokenTree.DELIMITED_CURLY, inside.getDelimiters());
+        assertEquals(TokenTree.SEPERATED_NA, inside.getSeperators());
+        assertEquals(0, inside.getTrees().size());
+    }
+
+    public void testParseArrayDecl() {
+        assertTrue(arrayDecl.isBranch());
+        assertEquals(TokenTree.SEPERATED_NA, arrayDecl.getSeperators());
+        TokenTree declType = arrayDecl.getTrees().get(0);
+        TokenTree arrayDesg = arrayDecl.getTrees().get(1);
+        TokenTree eq = arrayDecl.getTrees().get(2);
+        TokenTree array = arrayDecl.getTrees().get(3);
+
+        assertTrue(declType.isLeaf());
+        String[] declTypeTokens = { "int" };
+        assertEquals(Arrays.asList(declTypeTokens), declType.getTokens());
+
+        assertTrue(arrayDesg.isBranch());
+        assertEquals(TokenTree.DELIMITED_SQUARY, arrayDesg.getDelimiters());
+        assertEquals(TokenTree.SEPERATED_NA, arrayDesg.getSeperators());
+        assertEquals(0, arrayDesg.getTokens().size());
+
+        assertTrue(eq.isLeaf());
+        String[] eqTokens = { "=" };
+        assertEquals(Arrays.asList(eqTokens), eq.getTokens());
+
+        assertTrue(array.isBranch());
+        assertEquals(TokenTree.SEPERATED_COMMA, array.getSeperators());
+        assertEquals(TokenTree.DELIMITED_CURLY, array.getDelimiters());
+        assertEquals(3, array.getTrees().size());
+        String[] two = { "2" };
+        assertEquals(Arrays.asList(two), array.getTrees().get(0).getTokens());
+    }
+
+    public void testParseJavaFile() {
+        assertTrue(javaFile.isBranch());
+        assertEquals(TokenTree.DELIMITED_ROOT, javaFile.getDelimiters());
+        assertEquals(TokenTree.SEPERATED_SEMICOLON, javaFile.getSeperators());
+        TokenTree importStatement = javaFile.getTrees().get(0);
+        TokenTree classDecl = javaFile.getTrees().get(1);
+
+        assertTrue(importStatement.isLeaf());
+        assertEquals(6, importStatement.getTokens().size());
+
+        assertTrue(classDecl.isBranch());
+        assertEquals(4, classDecl.getTrees().size());
     }
 }
