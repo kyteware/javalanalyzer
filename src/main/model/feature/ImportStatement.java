e393673b07ebaa0b4b227180fc55fb315fd9c5cf
Kyteware <kyteware.biz@gmail.com>
2025-02-09 11:49:51 -0800

    build spec for import statement

---
+++ src/main/model/feature/ImportStatement.java
@@ -0,0 +1,24 @@
+package model.feature;
+
+import java.util.List;
+
+import model.ClassPath;
+import model.TokenTree;
+
+// representation of a java import statement
+public class ImportStatement {
+    // EFFECTS: build an import statement from a classpath
+    public ImportStatement(ClassPath classpath) {
+        // stub
+    }
+    
+    // MODIFIES: trees
+    // EFFECTS: attempt to build the feature from a slice of token trees, return null on failure
+    public ImportStatement tryBuilding(List<TokenTree> trees) {
+        return null;
+    }
+
+    public ClassPath getClassPath() {
+        return null;
+    }
+}
