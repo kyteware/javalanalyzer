92b4f862d8a69c7ebb02df607a2beecada6b08f3
Kyteware <kyteware.biz@gmail.com>
2025-02-10 15:11:29 -0800

    add some more methods to utils spec

--- src/main/model/Utils.java
+++ src/main/model/Utils.java
@@ -1,9 +1,22 @@
 package model;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
+// various utilities for parsing java code
 public class Utils {
+    // EFFECTS: true if the token is a valid java variable name or keyword
     public static boolean isWord(String token) {
         return Pattern.compile("([A-Z]|[a-z]|_)([A-Z]|[a-z]|[0-9]|_)*").matcher(token).matches();
+    }
+
+    // EFFECTS: attempt to take a token (leaf) from a list of tokentrees
+    public static String takeToken(List<TokenTree> tts, Integer current) {
+        return null; //stub
+    }
+
+    // EFFECTS: attempt to take a subtree (branch) from a list of tokentrees
+    public static TokenTree takeTree(List<TokenTree> tts, Integer current) {
+        return null; //stub
     }
 }
