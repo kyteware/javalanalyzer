e7997e915c18ca5e14fe6cdd8d5dcd1eb5bd5727
Kyteware <kyteware.biz@gmail.com>
2025-02-10 15:48:54 -0800

    add tests for extra utils

--- src/test/model/TestUtils.java
+++ src/test/model/TestUtils.java
@@ -2,6 +2,8 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -24,5 +26,69 @@ public class TestUtils {
         assertFalse(Utils.isWord("432"));
         assertFalse(Utils.isWord("sadfasdf.sadf"));
         assertFalse(Utils.isWord(" "));
+    }
+
+    @Test
+    public void takeTokenTest() throws Exception {
+        List<TokenTree> sample = TokenTree.parseJavaTokens(Tokenizer.tokenize(
+            "package ; {} "
+        )).getTrees();
+
+        assertEquals("package", Utils.takeToken(sample));
+        assertEquals(2, sample.size());
+
+        assertEquals(";", Utils.takeToken(sample));
+        assertEquals(1, sample.size());
+
+        try {
+            Utils.takeToken(sample);
+            fail();
+        } catch (UnexpectedToken e) {
+            // worked properly;
+        } catch (Exception e) {
+            fail();
+        }
+        assertEquals(1, sample.size());
+
+        sample.remove(0);
+        try {
+            Utils.takeToken(sample);
+            fail();
+        } catch (NoMoreTokens e) {
+            // worked properly;
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void takeTreeTest() throws Exception {
+        List<TokenTree> sample = TokenTree.parseJavaTokens(Tokenizer.tokenize(
+            "[] e"
+        )).getTrees();
+
+        assertEquals(TokenTree.parseJavaTokens(Tokenizer.tokenize("[]")).getTrees().get(0), Utils.takeTree(sample));
+        assertEquals(1, sample.size());
+
+        try {
+            Utils.takeTree(sample);
+            fail();
+        } catch (UnexpectedToken e) {
+            // worked properly;
+        } catch (Exception e) {
+            fail();
+        }
+        assertEquals(1, sample.size());
+
+        sample.remove(0);
+        try {
+            Utils.takeTree(sample);
+            fail();
+        } catch (NoMoreTokens e) {
+            // worked properly;
+        } catch (Exception e) {
+            fail();
+        }
+        assertEquals(0, sample.size());
     }
 }
