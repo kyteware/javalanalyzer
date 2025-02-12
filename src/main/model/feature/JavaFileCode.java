b22e7393793e99bc4a408ace4ce75d2a383b6c3c
Kyteware <kyteware.biz@gmail.com>
2025-02-11 22:42:23 -0800

    implement javafilecode

--- src/main/model/feature/JavaFileCode.java
+++ src/main/model/feature/JavaFileCode.java
@@ -1,31 +1,66 @@
 package model.feature;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.ClassPath;
 import model.NoMoreTokens;
 import model.TokenTree;
+import model.TooManyPackageDecls;
 import model.UnexpectedToken;
 
 // the code of an entire java file
 public class JavaFileCode {
+    List<ClassPath> imports;
+    ClassPath pkg;
+
     // EFFECTS: initialize the code of an empty java file
     public JavaFileCode() {
-        // stub
+        imports = new ArrayList<>();
+        pkg = null;
     }
 
     // EFFECTS: attempts to parse a list of token trees into a java file
-    public static JavaFileCode tryBuilding(List<TokenTree> trees) throws UnexpectedToken, NoMoreTokens {
-        return null; // stub
+    public static JavaFileCode tryBuilding(List<TokenTree> trees) throws TooManyPackageDecls {
+        JavaFileCode code = new JavaFileCode();
+        List<TokenTree> eaten = new ArrayList<>(trees);
+
+        while (eaten.size() > 0) {
+            try {
+                ImportStatement imported = ImportStatement.tryBuilding(eaten);
+                code.imports.add(imported.getClassPath());
+                continue;
+            }
+            catch (NoMoreTokens | UnexpectedToken e) {
+                // it wasn't an import statement
+            }
+
+            try {
+                PackageStatement pkg = PackageStatement.tryBuilding(eaten);
+                if (code.pkg == null) {
+                    code.pkg = pkg.getPath();
+                } else {
+                    throw new TooManyPackageDecls();
+                }
+                continue;
+            }
+            catch (NoMoreTokens | UnexpectedToken e) {
+                // it wasn't a package statement
+            }
+
+            eaten.remove(0);
+        }
+
+        return code;
     }
 
     // EFFECTS: gets all the classpaths that the java file imports
     public List<ClassPath> getImports() {
-        return null; //stub
+        return imports;
     }
 
     // EFFECTS: get the package path the file declares, or null if it doesn't declare one
     public ClassPath getPackage() {
-        return null;
+        return pkg;
     }
 }
