d5066e740efc0d4d740f8e55e86138cd8ded424d
Kyteware <kyteware.biz@gmail.com>
2025-02-11 21:48:07 -0800

    add spec for java file code

---
+++ src/main/model/feature/JavaFileCode.java
@@ -0,0 +1,29 @@
+package model.feature;
+
+import java.util.List;
+
+import model.ClassPath;
+import model.TokenTree;
+
+// the code of an entire java file
+public class JavaFileCode {
+    // EFFECTS: initialize the code of an empty java file
+    public JavaFileCode() {
+        // stub
+    }
+
+    // EFFECTS: attempts to parse a list of token trees into a java file
+    public static JavaFileCode tryBuilding(List<TokenTree> trees) {
+        return null; // stub
+    }
+
+    // EFFECTS: gets all the classpaths that the java file imports
+    public List<ClassPath> getImports() {
+        return null; //stub
+    }
+
+    // EFFECTS: get the package path the file declares, or null if it doesn't declare one
+    public List<ClassPath> getPackage() {
+        return null;
+    }
+}
