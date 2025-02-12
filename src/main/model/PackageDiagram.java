776bf117ac1de8be7ad5377e7ff965bef380b7e6
Kyteware <kyteware.biz@gmail.com>
2025-02-12 14:41:26 -0800

    add spec for package diagram

---
+++ src/main/model/PackageDiagram.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.List;
+import java.util.Map;
+
+// representation of a package diagram
+public class PackageDiagram {
+    // EFFECTS: build a package diagram from a list of classes in classpath form and all of their imports
+    public PackageDiagram(List<ClassPath> classes, Map<ClassPath, ClassPath> imports) {
+        // stub
+    }
+
+    // EFFECTS: get the classpaths contained in the diagram
+    public List<ClassPath> getClasses() {
+        return null; //stub
+    }
+
+    // EFFECTS: get all the imports made in the diagram
+    public Map<ClassPath, ClassPath> getImports() {
+        return null; //stub
+    }
+
+    // EFFECTS: create a string representation of the package diagram
+    public String stringify() {
+        return null; //stub
+    }
+}
