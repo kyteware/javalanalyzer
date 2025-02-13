93ba8a60b6ddddb597a318ac64c11738f745ecf3
Kyteware <kyteware.biz@gmail.com>
2025-02-12 22:38:22 -0800

    implement java project

--- src/main/model/JavaProject.java
+++ src/main/model/JavaProject.java
@@ -1,27 +1,46 @@
 package model;
 
 import java.nio.file.Path;
+import java.util.AbstractMap.SimpleEntry;
+
+import model.feature.JavaFileCode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // a representation of an entire java project (requires help loading java files from sys)
 public class JavaProject {
+    Path projectPath;
+    List<ClassPath> classes;
+    List<SimpleEntry<ClassPath, ClassPath>> imports;
+
     // EFFECTS: initialize a java project
     public JavaProject(Path path) {
-        // stub
+        projectPath = path;
+        classes = new ArrayList<>();
+        imports = new ArrayList<>();
     }
 
-    // EFFECTS: gets the path to the main code of the project (should append /src/main)
+    // EFFECTS: gets the path to the main code of the project
     public Path getMainPath() {
-        return null; // stub
+        return projectPath.resolve("src").resolve("main");
     }
 
     // EFFECTS: load a java file into the project
     // MODIFIES: this
-    public void loadJavaFile(Path path, String code) {
-        // stub
+    // REQUIRES: is actually a java file and named appropriately
+    public void loadJavaFile(Path path, String code) throws CodeException {
+        JavaFileCode compiled = JavaFileCode.tryBuilding(TokenTree.parseJavaTokens(Tokenizer.tokenize(code)).getTrees());
+        String className = path.getFileName().toString().split("\\.")[0];
+        ClassPath classPath = new ClassPath(compiled.getPackage().getPackagePath(), className);
+        classes.add(classPath);
+        for (ClassPath target : compiled.getImports()) {
+            imports.add(new SimpleEntry<>(classPath, target));
+        }
     }
 
     // EFFECTS: generate a package diagram for the java project
     public PackageDiagram genPackageDiagram() {
-        return null; //stub
+        return new PackageDiagram(classes, imports);
     }
 }
