9ff0be6a82929a72e4b8bdc44eeb5bbefafbac7f
Kyteware <kyteware.biz@gmail.com>
2025-02-12 22:11:38 -0800

    revise java project spec

--- src/main/model/JavaProject.java
+++ src/main/model/JavaProject.java
@@ -9,16 +9,18 @@ public class JavaProject {
         // stub
     }
 
-    // EFFECTS: gets the path to the main code of the project
+    // EFFECTS: gets the path to the main code of the project (should append /src/main)
     public Path getMainPath() {
         return null; // stub
     }
 
     // EFFECTS: load a java file into the project
+    // MODIFIES: this
     public void loadJavaFile(Path path, String code) {
         // stub
     }
 
+    // EFFECTS: generate a package diagram for the java project
     public PackageDiagram genPackageDiagram() {
         return null; //stub
     }
