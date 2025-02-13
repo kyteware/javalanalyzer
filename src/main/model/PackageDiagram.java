2a037df9d2c33069da5d07f1914f9baba230910a
Kyteware <kyteware.biz@gmail.com>
2025-02-12 22:04:02 -0800

    implement package diagram

--- src/main/model/PackageDiagram.java
+++ src/main/model/PackageDiagram.java
@@ -1,28 +1,77 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.AbstractMap.SimpleEntry;
 
 // representation of a package diagram
 public class PackageDiagram {
+    List<ClassPath> classes;
+    List<SimpleEntry<ClassPath, ClassPath>> imports;
+
     // EFFECTS: build a package diagram from a list of classes in classpath form and all of their imports
     public PackageDiagram(List<ClassPath> classes, List<SimpleEntry<ClassPath, ClassPath>> imports) {
-        // stub
+        this.classes = classes;
+        this.imports = imports;
     }
 
     // EFFECTS: get the classpaths contained in the diagram
     public List<ClassPath> getClasses() {
-        return null; //stub
+        return classes;
     }
 
     // EFFECTS: get all the imports made in the diagram
     public List<SimpleEntry<ClassPath, ClassPath>> getImports() {
-        return null; //stub
+        return imports;
     }
 
     // EFFECTS: create a string representation of the package diagram
     public String stringify() {
-        return null; //stub
+        List<List<String>> foundPackages = new ArrayList<>();
+
+        for (ClassPath classPath : classes) {
+            if (!foundPackages.contains(classPath.getPackagePath())) {
+                foundPackages.add(classPath.getPackagePath());
+            }
+        }
+
+        List<String> parts = new ArrayList<>();
+        for (List<String> pkg : foundPackages) {
+            parts.add(stringifyPackage(pkg));
+        }
+
+        return String.join("\n", parts);
+    }
+
+    private String stringifyPackage(List<String> pkg) {
+        List<ClassPath> foundClasses = new ArrayList<>();
+
+        for (ClassPath classPath : classes) {
+            if (classPath.getPackagePath().equals(pkg) && !foundClasses.contains(classPath)) {
+                foundClasses.add(classPath);
+            }
+        }
+
+        List<String> parts = new ArrayList<>();
+        parts.add("package " + new ClassPath(pkg, null).stringify() + ":\n");
+        for (ClassPath classPath : foundClasses) {
+            parts.add("\tclass " + classPath.getClassName() + "\n");
+            parts.add(stringifyImports(classPath));
+        }
+
+        return String.join("", parts);
+    }
+
+    private String stringifyImports(ClassPath classPath) {
+        List<String> parts = new ArrayList<>();
+
+        for (SimpleEntry<ClassPath, ClassPath> pair : imports) {
+            if (pair.getKey().equals(classPath)) {
+                ClassPath imported = pair.getValue();
+                parts.add("\t\t-> " + imported.stringify() + "\n");
+            }
+        }
+
+        return String.join("", parts);
     }
 }
