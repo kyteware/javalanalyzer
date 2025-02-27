5a511d8e84021f97292151130e956fab16276e93
Kyteware <kyteware.biz@gmail.com>
2025-02-26 21:55:28 -0800

    add helper fields to testjson

--- src/test/persistence/TestJson.java
+++ src/test/persistence/TestJson.java
@@ -2,9 +2,52 @@ package persistence;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 import model.JavaProject;
 
 public class TestJson {
+    public static List<JavaProject> expectedEmptyProjects;
+    public static List<JavaProject> expectedFullProjects;
+
+    public static void saturateExpectedProjects() {
+        expectedEmptyProjects = new ArrayList<>();
+        expectedFullProjects = new ArrayList<>();
+        
+        JavaProject p1 = new JavaProject(Paths.get("/path/to/Project1"));
+        try {
+            p1.loadJavaFile(
+                Paths.get("/path/to/Project1/src/model/Class1.java"), 
+                "package model; import util.MyUtils;"
+            );
+        } catch (Exception e) {
+            fail("this should NOT fail :(");
+        }
+        try {
+            p1.loadJavaFile(
+                Paths.get("/path/to/Project1/src/util/MyUtil.java"), 
+                "package util;"
+            );
+        } catch (Exception e) {
+            fail("this should NOT fail :(");
+        }
+
+        JavaProject p2 = new JavaProject(Paths.get("/path/to/Project2"));
+        try {
+            p2.loadJavaFile(
+                Paths.get("/path/to/Project2/src/ui/Main.java"), 
+                "package model;"
+            );
+        } catch (Exception e) {
+            fail("this should NOT fail :(");
+        }
+
+        expectedFullProjects.add(p1);
+        expectedFullProjects.add(p2);
+    }
+
     public static void assertJavaProjectEquals(JavaProject expected, JavaProject actual) {
         assertEquals(expected.getMainPath(), actual.getMainPath());
         assertEquals(expected.getClasses(), actual.getClasses());
