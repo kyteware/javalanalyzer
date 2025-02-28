e99164814b3d207e4022d2e5f0147c6212dcb741
Kyteware <kyteware.biz@gmail.com>
2025-02-28 12:39:34 -0800

    TEST: for jsonwriter (i added the spec a while ago)

---
+++ src/test/persistence/TestJsonWriter.java
@@ -0,0 +1,55 @@
+package persistence;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.JavaProject;
+
+public class TestJsonWriter {
+    JsonWriter writer;
+    JsonReader reader;
+
+    @BeforeEach
+    public void beforeEach() {
+        writer = new JsonWriter("./data/testWriter.json");
+        reader = new JsonReader("./data/testWriter.json");
+    }
+
+    @Test
+    public void emptyWrite() {
+        try {
+            writer.write(TestJson.expectedEmptyProjects);
+        } catch (WriteError e) {
+            fail("that should have worked");
+        }
+        
+        try {
+            List<JavaProject> res = reader.read();
+            TestJson.assertJavaProjectsEquals(TestJson.expectedEmptyProjects, res);
+        } catch (ReadError e) {
+            fail("that should have worked");
+        }
+    }
+
+    @Test
+    public void completeWrite() {
+        try {
+            writer.write(TestJson.expectedFullProjects);
+        } catch (WriteError e) {
+            fail("that should have worked");
+        }
+        
+        try {
+            List<JavaProject> res = reader.read();
+            TestJson.assertJavaProjectsEquals(TestJson.expectedFullProjects, res);
+        } catch (ReadError e) {
+            fail("that should have worked");
+        }
+    }
+}
