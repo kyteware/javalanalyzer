50de715225e156ff7e6a06e0046034083b99a564
Kyteware <kyteware.biz@gmail.com>
2025-03-04 12:31:41 -0800

    add skeleton for the gui

---
+++ src/main/ui/JavalanalyzerGui.java
@@ -0,0 +1,8 @@
+package ui;
+
+import javax.swing.JFrame;
+
+// the root of the gui
+public class JavalanalyzerGui extends JFrame {
+    
+}
---
+++ src/main/ui/MainGui.java
@@ -0,0 +1,9 @@
+package ui;
+
+// class containing entry point to gui
+public class MainGui {
+    // EFFECTS: run the gui
+    public static void main(String[] args) {
+        // stub
+    }
+}
